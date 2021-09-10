99679350e371f1bd68c45505710ea564ca38ffa0
yuramazurek <yura-mazurek@rambler.ru>
2021-09-10 20:20:36 +0300

    update DB, register repositories

---
+++ Migrations/20210910171810_init.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LearnEnglish.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lessons",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Level = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lessons", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Login = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Role = table.Column<int>(type: "int", nullable: false, defaultValue: 1),
+                    Language = table.Column<int>(type: "int", nullable: false, defaultValue: 2)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BankCards",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CardNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ValidityMonth = table.Column<int>(type: "int", nullable: false),
+                    ValidityYear = table.Column<int>(type: "int", nullable: false),
+                    OwnerId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BankCards", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BankCards_Users_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LessonUser",
+                columns: table => new
+                {
+                    LessonsId = table.Column<long>(type: "bigint", nullable: false),
+                    UsersId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LessonUser", x => new { x.LessonsId, x.UsersId });
+                    table.ForeignKey(
+                        name: "FK_LessonUser_Lessons_LessonsId",
+                        column: x => x.LessonsId,
+                        principalTable: "Lessons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LessonUser_Users_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserProfiles",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: true, computedColumnSql: "[FirstName] + ' ' + [LastName]"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AvatarUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserProfiles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserProfiles_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BankCards_OwnerId",
+                table: "BankCards",
+                column: "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LessonUser_UsersId",
+                table: "LessonUser",
+                column: "UsersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserProfiles_UserId",
+                table: "UserProfiles",
+                column: "UserId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BankCards");
+
+            migrationBuilder.DropTable(
+                name: "LessonUser");
+
+            migrationBuilder.DropTable(
+                name: "UserProfiles");
+
+            migrationBuilder.DropTable(
+                name: "Lessons");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,13 +1,21 @@
+using AutoMapper;
+using AutoMapper.Configuration;
+using LearnEnglish.EfStuff;
+using LearnEnglish.EfStuff.Repositories;
+using LearnEnglish.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using IConfiguration = Microsoft.Extensions.Configuration.IConfiguration;
 
 namespace LearnEnglish
 {
@@ -23,7 +31,66 @@ namespace LearnEnglish
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<LearnEnglishDbContext>
+                (options => options.UseSqlServer(connectionString));
+
+            //services.AddAuthentication(AuthName)
+            //    .AddCookie(AuthName, config =>
+            //    {
+            //        config.LoginPath = "/User/Login";
+            //        config.AccessDeniedPath = "/User/Denied";
+            //        config.Cookie.Name = "Smile";
+            //    });
+
+            RegisterRepositories(services);
+
+            RegisterMapper(services);
+
+            //services.NiceRegister<UserService>();
+
+            //services.NiceRegister<FileService>();
+
             services.AddControllersWithViews();
+
+            services.AddHttpContextAccessor();
+        }
+
+        private static void RegisterRepositories(IServiceCollection services)
+        {
+            var assembly = Assembly.GetAssembly(typeof(BaseRepository<>));
+
+            var repositories = assembly.GetTypes()
+                .Where(x =>
+                    x.IsClass
+                    && x.BaseType.IsGenericType
+                    && x.BaseType.GetGenericTypeDefinition() == typeof(BaseRepository<>));
+
+            foreach (var repositoryType in repositories)
+            {
+                services.RegisterAssistant(repositoryType);
+            }
+        }
+
+        private static void RegisterMapper(IServiceCollection services)
+        {
+            var provider = new MapperConfigurationExpression();
+
+            //provider.CreateMap<BankCard, BankCardGetViewModel>();
+
+            //provider.CreateMap<BankCardAddViewModel, BankCard>();
+
+            //provider.CreateMap<CourseAddViewModel, Course>();
+
+            //provider.CreateMap<RegistrationViewModel, User>();
+
+            //provider.CreateMap<UserProfileViewModel, UserProfile>();
+
+            var mapperConfiguration = new MapperConfiguration(provider);
+            var mapper = new Mapper(mapperConfiguration);
+
+            services.AddScoped<IMapper>(x => mapper);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
